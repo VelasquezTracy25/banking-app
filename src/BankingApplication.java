6fd2cdbcd46e836c31efcda77390b69c48c58334
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-09-18 22:28:46 -0500

    added BankAccount constructer, and transation methods

--- src/BankingApplication.java
+++ src/BankingApplication.java
@@ -5,3 +5,41 @@ public class BankingApplication {
     }
 
 }
+
+
+class BankAccount {
+    int balance;
+    int previousTransaction;
+    String customerName;
+    String customerId;
+
+    BankAccount(String custName, String custId){
+    customerName = custName;
+    customerId = custId;
+    }
+
+    void deposit(int amount) {
+        if (amount != 0) {
+            balance = balance + amount;
+            previousTransaction = amount;
+        }
+    }
+
+    void withdraw(int amount) {
+        if (amount != 0)
+            balance = balance - amount;
+    }
+
+    void getPreviousTransaction() {
+        if (previousTransaction > 0) {
+            System.out.println("Deposited: " + previousTransaction);
+        } else if (previousTransaction < 0) {
+        } else {
+            System.out.println("No transaction occurred.");
+        }
+    }
+
+    void showMenu(){
+
+    }
+}
